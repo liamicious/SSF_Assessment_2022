b581f3e0c730d2206e124581e6d49b5ec5d8ac22
liamicious <79270208+liamicious@users.noreply.github.com>
2022-01-18 16:49:30 +0800

    task 7 and 8 WIP

--- src/main/java/sg/nus/iss/ssftest/Configuration/BookRedisConfig.java
+++ src/main/java/sg/nus/iss/ssftest/Configuration/BookRedisConfig.java
@@ -2,8 +2,17 @@ package sg.nus.iss.ssftest.Configuration;
 
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
 import java.util.logging.Logger;
+
+import static sg.nus.iss.ssftest.Constants.*;
 
 @Configuration
 public class BookRedisConfig {
@@ -14,9 +23,39 @@ public class BookRedisConfig {
     private String redisHost;
 
     @Value("${spring.redis.port}")
-    private Optional<Integer> redisPort;
+    private Integer redisPort;
 
     @Value("${spring.redis.database}")
-    private int redisDatabase;
+    private Integer redisDatabase;
+
+    private final String redisPassword;
+
+    public BookRedisConfig() {
+        redisPassword = ENV_REDIS_PASSWORD;
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplateFactory() {
+
+        final RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName(redisHost);
+        redisConfig.setPort(redisPort);
+        if (null != redisPassword) {
+            redisConfig.setPassword(redisPassword);
+            logger.info("Set Redis password");
+        }
+        redisConfig.setDatabase(redisDatabase);
+
+        final JedisClientConfiguration jedisConfig = JedisClientConfiguration.builder().build();
+        final JedisConnectionFactory jedisFac = new JedisConnectionFactory(redisConfig, jedisConfig);
+        jedisFac.afterPropertiesSet();
+
+        final RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisFac);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+
+        return template;
+    }
 
 }
--- src/main/java/sg/nus/iss/ssftest/Repository/BookRepository.java
+++ src/main/java/sg/nus/iss/ssftest/Repository/BookRepository.java
@@ -13,47 +13,6 @@ import static sg.nus.iss.ssftest.Constants.*;
 
 import java.util.logging.Logger;
 
-@Configuration
 public class BookRepository {
-
-    private final Logger logger = Logger.getLogger(BookRepository.class.getName());
-
-    @Value("${spring.redis.host}")
-    private String redisHost;
-
-    @Value("${spring.redis.port}")
-    private Integer redisPort;
-
-    @Value("${spring.redis.database}")
-    private Integer redisDatabase;
-
-    private final String redisPassword = ""; // temmp set empty
-
-    // public BookRepository() {
-    // redisPassword = System.getenv(ENV_REDIS_PASSWORD);
-
-    @Bean
-    public RedisTemplate<String, String> redisTemplateFactory() {
-
-        final RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
-        redisConfig.setHostName(redisHost);
-        redisConfig.setPort(redisPort);
-        if (null != redisPassword) {
-            redisConfig.setPassword(redisPassword);
-            logger.info("Set Redis password");
-        }
-        redisConfig.setDatabase(redisDatabase);
-
-        final JedisClientConfiguration jedisConfig = JedisClientConfiguration.builder().build();
-        final JedisConnectionFactory jedisFac = new JedisConnectionFactory(redisConfig, jedisConfig);
-        jedisFac.afterPropertiesSet();
-
-        final RedisTemplate<String, String> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisFac);
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
-
-        return template;
-    }
 
 }
