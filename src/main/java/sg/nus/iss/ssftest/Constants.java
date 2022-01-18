7ab71cec19a2c7db94d560458a3b1809a54247ce
liamicious <79270208+liamicious@users.noreply.github.com>
2022-01-18 16:58:25 +0800

    task 7 and 8

--- src/main/java/sg/nus/iss/ssftest/Constants.java
+++ src/main/java/sg/nus/iss/ssftest/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
     public static final String URL_SEARCH_TITLE = "https://openlibrary.org/dev/docs/api/search.json";
     public static final String URL_SEARCH_BOOK = "https://openlibrary.org/dev/docs/api/books"; // View 3 (Book details)
 
+    public static final String ENV_REDIS_PASSWORD = System.getenv("REDIS_PW");
+
 }
