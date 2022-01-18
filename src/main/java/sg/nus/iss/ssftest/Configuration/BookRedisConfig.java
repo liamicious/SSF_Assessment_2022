aeb21ebf5e0e8dd80c34ba52f9b22fe0c806b967
liamicious <79270208+liamicious@users.noreply.github.com>
2022-01-18 09:22:29 +0800

    task 1 ssf test

--- src/main/java/sg/nus/iss/ssftest/Configuration/BookRedisConfig.java
+++ src/main/java/sg/nus/iss/ssftest/Configuration/BookRedisConfig.java
@@ -2,14 +2,7 @@ package sg.nus.iss.ssftest.Configuration;
 
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Configuration
--- src/main/java/sg/nus/iss/ssftest/Service/BookCacheService.java
+++ src/main/java/sg/nus/iss/ssftest/Service/BookCacheService.java
@@ -1,20 +1,10 @@
 package sg.nus.iss.ssftest.Service;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.util.List;
 import java.util.logging.Logger;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import sg.nus.iss.ssftest.Model.*;
 import sg.nus.iss.ssftest.Repository.*;
-
-import jakarta.json.Json;
-import jakarta.json.JsonArray;
-import jakarta.json.JsonObject;
-import jakarta.json.JsonReader;
 
 @Service
 public class BookCacheService {
